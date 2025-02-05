3ad1b0087a5f73c812ea6c69f55b5f9dc94ab2f1
IYATT-yx <iyatt@iyatt.com>
2025-02-05 10:08:07 +0800

    修改：适应支持使用日期时间作为版本号

--- building.ps1
+++ building.ps1
@@ -2,4 +2,5 @@ python -m venv venv
 .\venv\Scripts\activate
 pip install pyinstaller==6.11.1
 pip install -r .\requirements.txt
+python .\savebuildtime.py
 pyinstaller.exe -F -i .\icon.ico --add-data '.\icon.ico;.' -w .\quality-report-harvester.py
